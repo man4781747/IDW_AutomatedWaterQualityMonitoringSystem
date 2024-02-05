ca18e258df2c69863ae5d7279df75018856db910
man4781747 <man4781747@gmail.com>
2024-02-05 15:17:45 +0800

    時間模組增加時間位移功能

--- lib/TimeLibExternalFunction/TimeLibExternalFunction.h
+++ lib/TimeLibExternalFunction/TimeLibExternalFunction.h
@@ -15,29 +15,29 @@ inline String GetNowTimeString()
   return String(datetimeChar);
 }
 
-inline String GetDateString(String interval)
+inline String GetDateString(String interval, time_t timeShift=0)
 {
   time_t nowTime = now();
   char date[11];
   sprintf(date, "%04d%s%02d%s%02d",
-    year(nowTime), interval.c_str(), month(nowTime), interval.c_str(), day(nowTime)
+    year(nowTime+timeShift), interval.c_str(), month(nowTime+timeShift), interval.c_str(), day(nowTime+timeShift)
   );
   return String(date);
 }
 
-inline String GetTimeString(String interval)
+inline String GetTimeString(String interval, time_t timeShift=0)
 {
   time_t nowTime = now();
   char time_[11];
   sprintf(time_, "%02d%s%02d%s%02d",
-    hour(nowTime), interval.c_str(), minute(nowTime), interval.c_str(), second(nowTime)
+    hour(nowTime+timeShift), interval.c_str(), minute(nowTime+timeShift), interval.c_str(), second(nowTime+timeShift)
   );
   return String(time_);
 }
 
-inline String GetDatetimeString(String interval_date="-", String interval_middle=" ", String interval_time=":")
+inline String GetDatetimeString(String interval_date="-", String interval_middle=" ", String interval_time=":", time_t timeShift=0)
 {
-  return GetDateString(interval_date)+interval_middle+GetTimeString(interval_time);
+  return GetDateString(interval_date, timeShift)+interval_middle+GetTimeString(interval_time, timeShift);
 }
 
 #endif
