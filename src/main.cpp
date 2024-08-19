552f002ce53a457c079f3478ed752356d39c87bf
man4781747 <man4781747@gmail.com>
2024-08-19 10:05:04 +0800

    更新基礎市定

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,6 @@ void setup() {
   efuse_hal_get_minor_chip_version();
   efuse_ll_get_blk_version_major();
   efuse_ll_get_blk_version_minor();
-
   ESP_LOGI("MAIN", "儀器啟動中, 以下為相關基本資訊");
   ESP_LOGI("MAIN", "Cpu Frequency: %d MHz", getCpuFrequencyMhz());
   ESP_LOGI("MAIN", "Apb Frequency: %d MHz", getApbFrequency());
@@ -37,6 +36,7 @@ void setup() {
   ESP_LOGV("MAIN", "初始化SD卡");
   Device_Ctrl.AddNewOledLog("INIT_SD");
   Device_Ctrl.INIT_SD();
+  Device_Ctrl.CheckUpdate();
   ESP_LOGV("MAIN", "初始化SQLITE資料庫");
   Device_Ctrl.AddNewOledLog("INIT_SqliteDB");
   Device_Ctrl.INIT_SqliteDB();
@@ -51,16 +51,16 @@ void setup() {
   Device_Ctrl.UpdatePipelineConfigList();
   
   Device_Ctrl.AddNewOledLog("INIT_Motors");
-  Device_Ctrl.peristalticMotorsCtrl.INIT_Motors(PIN__Peristaltic_Motor_SHCP,PIN__Peristaltic_Motor_SHTP,PIN__Peristaltic_Motor_DATA,1);
+  Device_Ctrl.peristalticMotorsCtrl.INIT_Motors(PIN__Peristaltic_Motor_SHCP,PIN__Peristaltic_Motor_SHTP,PIN__Peristaltic_Motor_DATA,2);
   Device_Ctrl.AddNewOledLog("CreatePipelineFlowScanTask");
   Device_Ctrl.CreatePipelineFlowScanTask();
   Device_Ctrl.AddNewOledLog("CreateStepTasks");
   Device_Ctrl.CreateStepTasks();
   Device_Ctrl.AddNewOledLog("preLoadWebJSFile");
   Device_Ctrl.preLoadWebJSFile();
-  Device_Ctrl.AddNewOledLog("ConnectWiFi");
-  
-  Device_Ctrl.ConnectWiFi();
+  Device_Ctrl.AddNewOledLog("CreateWifiManager");
+  Device_Ctrl.CreateWifiManagerTask();
+  Device_Ctrl.AddNewOledLog("CreateWifiManager");
   Device_Ctrl.AddNewOledLog("INITWebServer");
   Device_Ctrl.INITWebServer();
   Device_Ctrl.AddNewOledLog("CreateOTAService");
@@ -74,31 +74,10 @@ void setup() {
   
   Device_Ctrl.InsertNewLogToDB(GetDatetimeString(), 1, "開機完畢");
   Device_Ctrl.CreateOledQRCodeTask();
-  Device_Ctrl.CreateWifiManagerTask();
   Device_Ctrl.all_INIT_done = true;
 }
 
 void loop() {
-  // if (Serial.available()){
-  //   String getMessage = Serial.readString();
-  //   if (getMessage == "restart") {
-  //     esp_restart();
-  //   }
-  // }
-  // vTaskDelay(5000/portTICK_PERIOD_MS);
-
   vTaskDelay(10000/portTICK_PERIOD_MS);
   Device_Ctrl.WriteSysInfo();
-  // for (const auto& pair : Device_Ctrl.TaskSettingMap) {
-  //   ESP_LOGI("Test", "%s %d/%d", pair.first.c_str(), pair.second.stack_depth-uxTaskGetStackHighWaterMark(pair.second.task_handle),pair.second.stack_depth);
-
-  // }
-  // Serial.println("===================");
-  // Serial.println(WiFi.BSSIDstr());
-  // Serial.println(WiFi.getMode());
-  // Serial.println(WiFi.getStatusBits());
-  // Serial.println(WiFi.broadcastIP());
-  // Serial.println(WiFi.localIP());
-  // Serial.println(WiFi.waitForConnectResult(1000));
-
 }
